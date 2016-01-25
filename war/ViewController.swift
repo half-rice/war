7820b3f95b11d88caece835459ab2299d3ad9f6d
surrus <xsurrus@gmail.com>
2016-01-24 23:42:31 -0700

    working version

--- war/ViewController.swift
+++ war/ViewController.swift
@@ -16,11 +16,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondCardImageView: UIImageView!
     @IBOutlet weak var dealButton: UIButton!
     
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var enemyScoreLabel: UILabel!
+    var playerScore:Int = 0
+    var enemyScore:Int = 0
+    
+    var cardNamesArray:[String] = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13", "card1"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        self.dealButton.setTitle("Play", forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,13 +34,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func dealButtonTapped(sender: UIButton) {
-        var firstRandomNumber = arc4random_uniform(13)+1
-        var firstCardString = String(format:"card%i", firstRandomNumber)
+        var firstRandomNumber = Int(arc4random_uniform(13))
+        var firstCardString = self.cardNamesArray[firstRandomNumber]
         self.firstCardImageView.image = UIImage(named: firstCardString)
         
-        var secondRandomNumber = arc4random_uniform(13)+1
-        var secondCardString = String(format:"card%i", secondRandomNumber)
+        var secondRandomNumber = Int(arc4random_uniform(13))
+        var secondCardString = self.cardNamesArray[secondRandomNumber]
         self.secondCardImageView.image = UIImage(named: secondCardString)
+        
+        if firstRandomNumber > secondRandomNumber {
+            self.playerScore += 1
+            self.playerScoreLabel.text = String(self.playerScore)
+        }
+        else if firstRandomNumber == secondRandomNumber {
+        
+        }
+        else {
+            self.enemyScore += 1
+            self.enemyScoreLabel.text = String(self.enemyScore)
+        }
     }
 
 }
